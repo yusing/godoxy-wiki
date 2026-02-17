83bd7d2aed2f4a4ab5f1882737ea0f6bed5e029d
yusing <yusing.wys@gmail.com>
2026-02-18 02:50:06 +0800

    Update Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,22 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      new URL("https://img.shields.io/github/v/release/**?style=flat-square"),
+    ],
+    dangerouslyAllowSVG: true,
+  },
   async rewrites() {
     return [
       {
-        source: '/docs/:path*.mdx',
-        destination: '/llms.mdx/docs/:path*',
+        source: "/docs/:path*.mdx",
+        destination: "/llms.mdx/docs/:path*",
       },
     ];
   },
