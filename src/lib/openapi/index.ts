2bfb5baec7bad56ec6bb86f71edfa68f1d1659a3
yusing <yusing.wys@gmail.com>
2026-02-24 09:00:40 +0800

    fix(openapi): enhance api json path with resolve

--- src/lib/openapi/index.ts
+++ src/lib/openapi/index.ts
@@ -1,6 +1,7 @@
 import { createOpenAPI } from 'fumadocs-openapi/server';
+import {resolve} from 'node:path';
 
 export const openapi = createOpenAPI({
   // the OpenAPI schema, you can also give it an external URL.
-  input: ['./public/api.json'],
+  input: [resolve('./public/api.json')],
 });
